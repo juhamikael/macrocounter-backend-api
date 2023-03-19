0e393a1ec377985e790c16c8b7725e366a34b14a
Juhamikael <contact.juhamusic@gmail.com>
2023-03-20 01:56:26 +0200

    Update headers & Origins

--- main.py
+++ main.py
@@ -16,12 +16,18 @@ origins = [
     "https://projects.macrocounter.juhamikael.me/*",
     "https://macrocounter.juhamikael.me*",
     "http://*.macrocounter.juhamikael.me/*",
+    "https://macrocounter.juhamikael.me"
     "*.macrocounter.juhamikael.me/*",
     "http://localhost:3000",
     "http://localhost:*",
     "http://localhost:8080",
     "http://localhost:5050",
     "http://localhost:8000",]
+
+app.add_middleware(CORSMiddleware,
+                   allow_origins=origins,
+                   allow_methods=["*"],
+                   allow_headers=["*"], )
 
 app.add_middleware(
     DBSessionMiddleware,
@@ -30,11 +36,6 @@ app.add_middleware(
 app.include_router(user.router)
 app.include_router(food.router)
 app.include_router(food_eaten.router)
-
-app.add_middleware(CORSMiddleware,
-                   allow_origins=origins,
-                   allow_methods=["*"],
-                   allow_headers=["*"], )
 
 
 @app.get("/", tags=["ROOT"], summary="Root redirect")
