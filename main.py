40ea8e401cfbd475bb96e593979361eaf6406602
Juhamikael <contact.juhamusic@gmail.com>
2023-03-20 02:51:01 +0200

    Try * origins

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ origins = [
     "http://localhost:8000",]
 
 app.add_middleware(CORSMiddleware,
-                   allow_origins=origins,
+                   allow_origins=["*"],
                    allow_methods=["*"],
                    allow_headers=["*"], )
 
